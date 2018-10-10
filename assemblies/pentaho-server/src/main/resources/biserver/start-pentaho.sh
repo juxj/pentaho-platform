e0c48063915d47092332f48e10984208018973d0
Tony Ju <tony.ju@me.com>
2018-10-10 18:19:12 +0800

    change db connection to mysql

--- assemblies/pentaho-server/src/main/resources/biserver/start-pentaho.sh
+++ assemblies/pentaho-server/src/main/resources/biserver/start-pentaho.sh
@@ -51,5 +51,7 @@ if [ "$errCode" = 0 ]; then
   CATALINA_OPTS="-Xms2048m -Xmx6144m -XX:MaxPermSize=256m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -Dfile.encoding=utf8 -DDI_HOME=\"$DI_HOME\""
   export CATALINA_OPTS
   JAVA_HOME=$_PENTAHO_JAVA_HOME
+  rm -f ../logs/*
   sh startup.sh
+  tail -f ../logs/catalina.out
 fi
